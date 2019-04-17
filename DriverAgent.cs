0e24d37abb0a334299efad1607256df63f6d5f76
Ryan <galearyan12@gmail.com>
2019-04-17 21:01:09 +0200

    ayy

---
+++ DriverAgent.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using UnityStandardAssets.Vehicles.Car;
+
+public class DriverAgent : Agent {
+    [SerializeField] LayerMask SensorMask; // Defines the layer of the walls ("Wall")
+    Rigidbody rBody;
+    float currentTime;
+    private CarController m_Car; // the car controller we want to use
+    double distanceRight;
+    double distanceLeft;
+    double distanceFront;
+    double distanceBack;
+    GameObject LineBlock;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        m_Car = GetComponent<CarController>();
+        currentTime = 0;
+        LineBlock = GameObject.Find("LineBlock");
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > 30)
+        {
+            AddReward(-0.1f);
+            Done();
+            Debug.Log("Out of Time");
+        }
+
+        if(this.gameObject.transform.position.y > 3)
+        {
+            AddReward(-0.01f);
+            Done();
+            Debug.Log("Above track height");
+        }
+
+        castRayRight();
+        castRayLeft();
+        castRayForward();
+        castRayBack();
+        //Debug.Log("Distance right : " + distanceRight.ToString());
+    }
+    public Transform Target;
+    public override void AgentReset()
+    {
+
+            // If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.position = new Vector3(63.44f, 2.52f, -145.75f);
+            this.transform.rotation = new Quaternion(0, 80.93f, 0, 90f);
+            this.gameObject.GetComponent<Timer>().Restart();
+            Target = GameObject.Find("Checkpoint1").transform;
+            currentTime = 0;
+    }
+
+    public override void CollectObservations()
+    {
+        // Target and Agent positions
+        //AddVectorObs(Target.position);
+        //AddVectorObs(this.transform.position);
+
+        // Agent velocity
+        AddVectorObs((float)distanceRight);
+        AddVectorObs((float)distanceLeft);
+        AddVectorObs((float)distanceFront);
+        AddVectorObs((float)distanceBack);
+    }
+
+    //public float speed = 10;
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        
+        float h = vectorAction[0];
+        float v = vectorAction[1];
+        
+        m_Car.Move(h, v, v, 0);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.position,
+                                                  Target.position);
+
+        // Reached target
+        if (distanceToTarget < 0.01f)
+        {
+            AddReward(0.5f);
+            currentTime = 0;
+        }
+
+        // Fell off platform
+        if (this.transform.position.y < 0)
+        {
+            Done();
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "wall")
+        {
+            Debug.Log("Left Wall");
+            AddReward(-0.01f);
+            Done();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "sidewall")
+        {
+            Debug.Log("Hit Wall");
+            AddReward(-0.01f);
+            Done();
+        }
+
+        if (other.gameObject.tag == "wall")
+        {
+            Debug.Log("Left Track");
+            AddReward(-0.01f);
+            Done();
+        }
+
+        if (other.gameObject.name == GameObject.Find("TimerStop").name && this.gameObject.GetComponent<Timer>().isGameFinished)
+        {
+            Done();
+        }
+    }
+
+    void castRayRight()
+    {
+        RaycastHit hit;
+        Ray rightRay = new Ray(LineBlock.transform.position, LineBlock.transform.right);
+
+        Debug.DrawRay(LineBlock.transform.position, LineBlock.transform.right, Color.green);
+
+        if (Physics.Raycast(rightRay, out hit))
+        {
+            distanceRight = hit.distance;
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+    void castRayLeft()
+    {
+        RaycastHit hit;
+        Ray leftRay = new Ray(LineBlock.transform.position, -LineBlock.transform.right);
+
+        Debug.DrawRay(LineBlock.transform.position, -LineBlock.transform.right, Color.blue);
+
+        if (Physics.Raycast(leftRay, out hit))
+        {
+            distanceLeft = hit.distance;
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+    void castRayForward()
+    {
+        RaycastHit hit;
+        Ray forwardRay = new Ray(LineBlock.transform.position, LineBlock.transform.forward);
+
+        Debug.DrawRay(LineBlock.transform.position, LineBlock.transform.forward, Color.green);
+
+        if (Physics.Raycast(forwardRay, out hit))
+        {
+            distanceFront = hit.distance;
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+    void castRayBack()
+    {
+        RaycastHit hit;
+        Ray backRay = new Ray(LineBlock.transform.position, -LineBlock.transform.forward);
+
+        Debug.DrawRay(LineBlock.transform.position, -LineBlock.transform.forward, Color.blue);
+
+        if (Physics.Raycast(backRay, out hit))
+        {
+            distanceBack = hit.distance;
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public GameObject StartTimer;
+    public GameObject StopTimer;
+    public GameObject[] Checkpoints;
+    public Text StopWatch;
+    public Text CheckpointTracker;
+    int checkpoints;
+    bool isTimerStarted = false;
+    public bool isGameFinished = false;
+    float currentTime;
+    
+
+    // Use this for initialization
+    void Start () {
+        currentTime = 0;
+        checkpoints = 0;
+        isTimerStarted = false;
+        isGameFinished = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isTimerStarted && !isGameFinished)
+        {
+            currentTime += Time.deltaTime;
+            StopWatch.text = currentTime.ToString("0.00");
+        }
+	}
+
+    public void Restart()
+    {
+        currentTime = 0;
+        checkpoints = 0;
+        isTimerStarted = false;
+        isGameFinished = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.gameObject.name == StartTimer.name)
+        {
+            isTimerStarted = true;
+            Debug.Log("Timer Started");
+        }
+
+        if (other.gameObject.name == StopTimer.name && isTimerStarted && checkpoints == 8)
+        {
+            isTimerStarted = false;
+            isGameFinished = true;
+            Debug.Log("Timer Stopped");
+        }
+
+        #region Checkpoints
+
+            if (other.gameObject.name == Checkpoints[0].name && checkpoints == 0)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[1].transform;
+                Debug.Log(Checkpoints[1].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+            }
+
+            if (other.gameObject.name == Checkpoints[1].name && checkpoints == 1)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[2].transform;
+                Debug.Log(Checkpoints[2].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[2].name && checkpoints == 2)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[3].transform;
+                Debug.Log(Checkpoints[3].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[3].name && checkpoints == 3)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[4].transform;
+                Debug.Log(Checkpoints[4].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[4].name && checkpoints == 4)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[5].transform;
+                Debug.Log(Checkpoints[5].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[5].name && checkpoints == 5)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[6].transform;
+                Debug.Log(Checkpoints[6].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[6].name && checkpoints == 6)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = Checkpoints[7].transform;
+                Debug.Log(Checkpoints[7].name);
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+            if (other.gameObject.name == Checkpoints[7].name && checkpoints == 7)
+            {
+                checkpoints++;
+                updateCheckpoints();
+                this.gameObject.GetComponent<DriverAgent>().Target = StopTimer.transform;
+            this.gameObject.GetComponent<DriverAgent>().AddReward(0.5f);
+        }
+
+        #endregion
+
+    }
+    void updateCheckpoints()
+    {
+        CheckpointTracker.text = (checkpoints + "/8");
+    }
+}
